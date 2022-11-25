4ad0e509f4439005a1bde57721a8a3064f1f2922
Sunaid Ahmed <sunaid.ahmed@c3spectra.com>
2022-11-25 18:53:32 +0500

    New

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ async function run() {
   try {
     const folder = getInput('folder');
     const bucket = getInput('bucket');
+    const destinationDir = getInput('destination-dir');
+    const vsersionDir = getInput('version-dir');
     const distId = getInput('dist-id');
     const invalidation = getInput('invalidation');
 
